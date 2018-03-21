5eb56bfd1cb4d29e2ddb8ca0c4363a3e6f6ec771
Michael Herger <michael@herger.net>
2018-03-21 12:12:51 +0100

    Modularize the Docker build script

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -6,20 +6,19 @@ cp -r /src/src /src/*.rs /src/Cargo.* .
 DESTDIR=/src/releases
 
 mkdir -p $DESTDIR/i386-linux
+rm -f $DESTDIR/i386-linux/*
+
 mkdir -p $DESTDIR/arm-linux
+rm -f $DESTDIR/arm-linux/*
 
-cargo build --release --target x86_64-unknown-linux-musl \
-	&& strip /build/x86_64-unknown-linux-musl/release/spotty \
-	&& cp /build/x86_64-unknown-linux-musl/release/spotty $DESTDIR/i386-linux/spotty-x86_64
+function build {
+	echo Building for $1 to $3...
+	cargo build --release --target $1 \
+		&& $2 /build/$1/release/spotty \
+		&& cp /build/$1/release/spotty $DESTDIR/$3
+}
 
-cargo build --release --target i686-unknown-linux-musl \
-	&& strip /build/i686-unknown-linux-musl/release/spotty \
-	&& cp /build/i686-unknown-linux-musl/release/spotty $DESTDIR/i386-linux/spotty
-
-cargo build --release --target aarch64-unknown-linux-gnu \
-	&& aarch64-linux-gnu-strip /build/aarch64-unknown-linux-gnu/release/spotty \
-	&& cp /build/aarch64-unknown-linux-gnu/release/spotty $DESTDIR/arm-linux/spotty-aarch64
-
-cargo build --release --target arm-unknown-linux-gnueabihf \
-	&& arm-linux-gnueabihf-strip /build/arm-unknown-linux-gnueabihf/release/spotty \
-	&& cp /build/arm-unknown-linux-gnueabihf/release/spotty $DESTDIR/arm-linux/spotty-hf
+build x86_64-unknown-linux-musl strip i386-linux/spotty-x86_64
+build i686-unknown-linux-musl strip i386-linux/spotty
+build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip arm-linux/spotty-aarch64
+build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip arm-linux/spotty-hf
