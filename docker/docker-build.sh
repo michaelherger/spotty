b676bbefdcd0d6a27b54c96a4f59c9255124e014
Michael Herger <michael@herger.net>
2018-03-25 22:19:53 +0200

    Disable arm build from Docker, as it fails to run on 1st gen. Pi under piCorePlayer

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -20,5 +20,6 @@ function build {
 
 build x86_64-unknown-linux-musl strip i386-linux/spotty-x86_64
 build i686-unknown-linux-musl strip i386-linux/spotty
+# binary built in docker would not run on 1st gen. Pi running pCP. Whysoever.
+# build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip arm-linux/spotty-hf
 build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip arm-linux/spotty-aarch64
-build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip arm-linux/spotty-hf
