7956b4f5e1d2202492ab79aeaf01cdd3dbd2ee2f
Michael Herger <michael@herger.net>
2018-03-26 11:12:29 +0200

    Add (disabled) configuration for armel to Dockerfile. It's disabled for now, as the resulting binary would not run on Synology because of some glibc mismatch...

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -20,6 +20,10 @@ function build {
 
 build x86_64-unknown-linux-musl strip i386-linux/spotty-x86_64
 build i686-unknown-linux-musl strip i386-linux/spotty
+build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip arm-linux/spotty-aarch64
+
 # binary built in docker would not run on 1st gen. Pi running pCP. Whysoever.
 # build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip arm-linux/spotty-hf
-build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip arm-linux/spotty-aarch64
+
+# armel binary wouldn't run on eg. Synology due to wrong glibc version?
+# build arm-unknown-linux-gnueabi arm-linux-gnueabi-strip arm-linux/spotty
